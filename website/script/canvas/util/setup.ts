939cee8a2930b799f1cc43ac67c270f6da2c9a34
Raffael Schäfer <raffael.elias.schaefer@gmail.com>
2021-05-25 23:06:55 +0200

    Code works now with typescript but still needs more improvements

---
+++ website/script/canvas/util/setup.ts
@@ -0,0 +1,10 @@
+export function setupHighDpiCanvas (canvas: HTMLCanvasElement) {
+    let dpr = window.devicePixelRatio || 1;
+    let rect = canvas.getBoundingClientRect();
+    canvas.width = rect.width * dpr;
+    canvas.height = rect.height *dpr;
+    let ctx = canvas.getContext('2d');
+    // @ts-ignore
+    ctx.scale(dpr, dpr);
+    return ctx;
+}
