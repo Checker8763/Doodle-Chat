a800c60240f38c7ca5e58b0b00c2710a128cfde1
Raffael Elias Schäfer <s20a3c@umwelt-campus.de>
2022-09-06 13:02:03 +0200

    Sass restructuring complete
    The restructuring of the style.sass file in to separate files is complete. The rework of the client can begin

--- client/typescript/ui/window/close.ts
+++
@@ -1,7 +0,0 @@
-export function visibilityHandler (element: any) {
-    if (element.style.visibility === 'visible') {
-        element.style.visibility = 'hidden';
-    } else {
-        element.style.visibility = 'visible';
-    }
-}
