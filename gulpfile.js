71a25c68783bd4e7ed6f8910bacbb7437c257dba
Raffael Elias Schäfer <s20a3c@umwelt-campus.de>
2022-09-10 18:07:09 +0200

    Gulp Pipeline Upgrade & work on the Sidebar
    I upgraded the Gulp pipeline to compile everything to the ./dist folder and I added an initialization command, also more stuff gets compressed for better loading time. I'm also working on a better sidebar for the UI.
    
    Btw. I'm sorry Checker8763, I messed up your package-lock.json file again 😅

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,80 @@
+/*Imports*/
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const sourcemaps = require('gulp-sourcemaps');
 const cleanCSS = require('gulp-clean-css');
-const purgecss = require('gulp-purgecss');
+const purgeCss = require('gulp-purgecss');
 const ts = require("gulp-typescript");
 const tsProject = ts.createProject("tsconfig.json");
 const uglify = require('gulp-uglify');
+const htmlMin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
 
-const compileSass = () => {
-    return gulp.src("./client/sass/**/*.sass")
-        .pipe(sass()).pipe(gulp.dest("./client/public/css/"));
+/*Config*/
+const config = {
+    in: {
+        client: "./client",
+        server: "./server"
+    },
+    out: "./dist"
 }
 
-const minifyCss = () => {
-    return gulp.src("./client/public/css/**/*.css")
-        .pipe(sourcemaps.init())
-        .pipe(cleanCSS())
-        .pipe(sourcemaps.write("./"))
-        .pipe(gulp.dest("./client/public/css/"));
+/*Tasks*/
+const compileSass = () => {
+    return gulp.src(`${config.in.client}/sass/**/*.sass`)
+        .pipe(sass()).pipe(gulp.dest(`${config.out}/css`));
 }
 
 const purge = () => {
-    return gulp.src('./client/public/css/**/*.css')
-        .pipe(purgecss({
-            content: ["./client/public/*.html"]
+    return gulp.src(`${config.out}/css/**/*.css`)
+        .pipe(purgeCss({
+            content: [`${config.in.client}/public/*.html`]
         }))
-        .pipe(gulp.dest('./client/public/css/'))
+        .pipe(gulp.dest(`${config.out}/css`))
+}
+
+const minifyCss = () => {
+    return gulp.src(`${config.out}/css/**/*.css`)
+        .pipe(sourcemaps.init())
+        .pipe(cleanCSS())
+        .pipe(sourcemaps.write("./"))
+        .pipe(gulp.dest(`${config.out}/css/`));
+}
+
+const compileHTML = () => {
+    return gulp.src(`${config.in.client}/public/**/*.html`)
+        .pipe(htmlMin({ collapseWhitespace: true }))
+        .pipe(gulp.dest(`${config.out}/`));
 }
 
 const compileTypescript = () => {
-    return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("./client/public/js/"));
+    return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest(`${config.out}/js/`));
 }
 
 const compress = () => {
-    return gulp.src("./client/public/js/**/*.js").pipe(sourcemaps.init()).pipe(uglify()).pipe(sourcemaps.write("./")).pipe(gulp.dest("./client/public/js/"))
+    return gulp.src(`${config.out}/js/**/*.js`).pipe(sourcemaps.init()).pipe(uglify()).pipe(sourcemaps.write("./")).pipe(gulp.dest(`${config.out}/js/`))
+}
+
+const optimizeAssets = () => {
+    return gulp.src(`${config.in.client}/public/assets/**/*.{png,gif,jpg,svg}`)
+        .pipe(imagemin())
+        .pipe(gulp.dest(`${config.out}/assets/`))
 }
 
 const watch = () => {
-    gulp.watch("./client/sass/**/*.sass", gulp.series(compileSass, purge, minifyCss));
-    gulp.watch("./client/public/**/*.html").on("change", gulp.series(compileSass, purge, minifyCss));
-    gulp.watch("./client/typescript/**/*.ts").on("change", gulp.series(compileTypescript, compress));
+    gulp.watch(`${config.in.client}/sass/**/*.sass`).on("change", gulp.series(compileSass, purge, minifyCss));
+    gulp.watch(`${config.in.client}/public/**/*.html`).on("change", gulp.series(compileHTML, compileSass, purge, minifyCss));
+    gulp.watch(`${config.in.client}/public/assets/**/*.{png,gif,jpg,svg}`).on("change", gulp.series(optimizeAssets));
+    gulp.watch(`${config.in.client}/typescript/**/*.ts`).on("change", gulp.series(compileTypescript, compress));
 }
 
+/*Exports*/
 exports.compileSass = compileSass;
 exports.purge = purge;
 exports.minifyCss = minifyCss;
 exports.compileTypescript = compileTypescript;
 exports.compress = compress;
+exports.compileHTML = compileHTML;
+exports.optimizeAssets = optimizeAssets;
 exports.watch = watch;
+exports.run = gulp.parallel(gulp.series(compileHTML, compileSass, purge, minifyCss), optimizeAssets, gulp.series(compileTypescript, compress));
