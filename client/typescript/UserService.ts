4fdfc430c1ade3998124446dddea9bbab94fed1a
Raffael Elias Schäfer <s20a3c@umwelt-campus.de>
2022-09-11 22:22:40 +0200

    minor updates
    Minor updates and style changes

--- client/typescript/UserService.ts
+++ client/typescript/UserService.ts
@@ -1,8 +1,12 @@
+const config = {
+    url: "https://jsonplaceholder.typicode.com/users"
+}
+
 class UserService {
     static getFriends = () => new Promise(async (resolve, reject) => {
         try {
             //@ts-ignore
-            const res = await axios.get("https://jsonplaceholder.typicode.com/users")
+            const res = await axios.get(config.url)
             resolve(res.data)
         } catch (err) {
             reject(err)
